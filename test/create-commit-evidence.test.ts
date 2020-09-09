1c2a02ebc7d0ca81d2cf8288092488007184d3fa
Felix Kirchengast <mail@felixkirchengast.at>
2020-09-09 21:36:17 +0200

    TODO

--- test/create-commit-evidence.test.ts
+++ test/create-commit-evidence.test.ts
@@ -7,4 +7,5 @@ test('write success', async () => {
   expect(output).toContain('Wrote commit evidence {"commitHash":"');
   expect(output).toContain('"} to \'/');
   expect(output).toContain("/test/empty-test-project/commit-evidence.json'");
+  // TODO: Check file exists, verify content.
 });
