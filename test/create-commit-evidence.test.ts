b4a82bdc084d3d6bb8fde9905b04a1ebf92b7439
Felix Kirchengast <mail@felixkirchengast.at>
2020-09-09 20:40:00 +0200

    Fix create test

--- test/create-commit-evidence.test.ts
+++ test/create-commit-evidence.test.ts
@@ -1,8 +1,10 @@
 import { runCapSafe } from './test-util';
 
 test('valid dir', async () => {
-  const output = await runCapSafe(`create-commit-evidence test/test-projects/`);
+  const output = await runCapSafe(
+    `create-commit-evidence test/empty-test-project/`,
+  );
   expect(output).toBe(
-    'TODO: Implement createCommitEvidence test/test-projects/\n',
+    'TODO: Implement createCommitEvidence test/empty-test-project/\n',
   );
 });
